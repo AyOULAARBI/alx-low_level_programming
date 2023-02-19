7f6452d0c0af9dfc240c96cf55c35b3303e327a5
ayoub <ayoub6oulaarbi6@gmail.com>
2023-02-19 07:12:05 -0800

    message

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/*
+ *main function
+ *returns 0 after it prints out a string
+ * */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
